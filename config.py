3ea4509e3b4fb1ed786be7f19aa9e4f590b587d8
00fiveshooter <kf.hedrick@icloud.com>
2025-07-14 01:03:14 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,2 @@
+BOT_TOKEN = '7917896559:AAGGvVgTf_wT97slR1vHEvp2CweTzfTPp6Y'  # Replace with your actual Telegram Bot Token
+ADMIN_ID = 7388684977               # Replace with your Telegram user ID (integer)
